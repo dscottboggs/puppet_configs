013c10c0d2ebe8e86ba935144f5ee6467476d14f
scott <scott@tams.tech>
2018-03-04 14:00:20 -0500

    Fixed variables to represent older version from distro release

--- apt-sources.pp
+++ apt-sources.pp
@@ -2,12 +2,12 @@ $home_folder = '/home/scott'
 $puppet_dir = "${home_folder}/puppet"
 $sourcefile = "${puppet_dir}/sources.list.erb"
 
-if $os['name'] == 'linux' {
-  if $os['distro']['family'] == 'ubuntu'{
+if  $::kernel == 'linux' {
+  if $::osfamily == 'ubuntu'{
     $mirror='http://us.archive.ubuntu.com/ubuntu/'
     $suites='main restricted universe multiverse'
   }
-  elsif $os['distro']['family'] == 'debian' {
+  elsif $::osfamily == 'debian' {
     $mirror='http://ftp.us.debian.org/debian/'
     $suites='main contrib non-free'
   }
