4a3e99a93864e2c77169916831068fa27d8bfe88
scott <scott@tams.tech>
2018-03-05 02:19:49 -0500

    Added sudoers manifest

---
+++ sudoers.pp
@@ -0,0 +1,21 @@
+$home_folder = '/home/scott'
+$puppet_dir = "${home_folder}/puppet"
+package { 'sudo':
+  ensure => installed,
+}
+file { '/etc/sudoers':
+  ensure  => file,
+  mode    => '0640',
+  group   => '0',
+  owner   => '0',
+  content => "# sudoers file
+# this file is managed by puppet. Do not edit.
+# see `man 5 sudoers` for information on this config,
+# edit it in ${puppet_dir}/sudoers.pp to change the options.
+Defaults        env_reset
+Defaults        mail_badpass
+Defaults        secure_path=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\"
+root            ALL=(ALL:ALL) ALL
+%sudo           ALL=(ALL:ALL) ALL
+"
+}
