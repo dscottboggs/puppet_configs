53b97364faf8f4fa19f87357de559d26c87528b9
scott <scott@tams.tech>
2018-03-05 19:10:09 -0500

    I think I'm going to abandon these. They're becoming a time sink.

--- apt-sources.pp
+++ apt-sources.pp
@@ -3,18 +3,18 @@ $puppet_dir = "${home_folder}/puppet"
 $sourcefile = "${puppet_dir}/sources.list.erb"
 
 if  $::kernel == 'linux' {
-  if $::software_platform == 'Ubuntu'{
-    $mirror='http://us.archive.ubuntu.com/ubuntu/'
-    $suites='main restricted universe multiverse'
+  if $::osfamily == 'Debian' {
+    if $software_platform == 'Ubuntu'{
+      $mirror='http://us.archive.ubuntu.com/ubuntu/'
+      $suites='main restricted universe multiverse'
+    }
+    elsif $software_platform == 'Debian' {
+      $mirror='http://ftp.us.debian.org/debian/'
+      $suites='main contrib non-free'
+    }
+    file { '/etc/apt/sources.list':
+      ensure  => file,
+      mode    => '0644',
+      content => template($sourcefile)
+    }
   }
-  elsif $::software_platform == 'Debian' {
-    $mirror='http://ftp.us.debian.org/debian/'
-    $suites='main contrib non-free'
-  }
-}
-
-file { '/etc/apt/sources.list':
-  ensure  => file,
-  mode    => '0644',
-  content => template($sourcefile)
-}
