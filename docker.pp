f22393a1b28701b2cfd34ab992ddbc3d9788eefc
scott <scott@tams.tech>
2018-04-14 13:25:21 -0400

    I'm not sure if this will work, but the regex was not allowing /'s in the link

--- docker.pp
+++ docker.pp
@@ -23,7 +23,7 @@ if $::osfamily == 'Debian' {
     repos    => 'stable',
     key      => {
       id     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
-      server => 'https://download.docker.com/linux/debian/gpg',
+      server => 'https://download.docker.com',
     },
     include  => {
       'src' => false,
