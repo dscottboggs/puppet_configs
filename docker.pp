b3de8822ad8b2f9abbf2292000ec3caec4d65a89
scott <scott@tams.tech>
2018-04-14 13:11:49 -0400

    Added docker manifest

---
+++ docker.pp
@@ -0,0 +1,37 @@
+include apt
+
+if $::osfamily == 'Debian' {
+  package { 'apt-transport-https':
+    ensure => latest,
+  }
+  package { 'ca-certificates':
+    ensure => latest,
+  }
+  package { 'curl':
+    ensure => latest,
+  }
+  package { 'gnupg2':
+    ensure => latest,
+  }
+  package { 'software-properties-common':
+    ensure => latest,
+  }
+  apt::source { 'docker-ce':
+    comment  => 'The official docker repository.',
+    location => 'https://download.docker.com/linux/debian',
+    release  => $facts['os']['lsb']['distcodename'],
+    repos    => 'stable',
+    key      => {
+      id     => '9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88',
+      server => 'https://download.docker.com/linux/debian/gpg',
+    },
+    include  => {
+      'src' => false,
+      'deb' => true
+    }
+  }
+  notify => Exec['apt_update']
+  package { 'docker-ce':
+    ensure => latest,
+  }
+}
