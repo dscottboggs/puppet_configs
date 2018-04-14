09a1b8485547eaf1069aa20eee08006367fb1dac
scott <scott@tams.tech>
2018-04-14 14:46:49 -0400

    manifests for X2go client and server.

---
+++ x2go-server.pp
@@ -0,0 +1,37 @@
+package { 'apt-transport-https':
+  ensure => installed,
+}
+apt::source { 'x2go'
+  comment  => 'The x2go repository.',
+  key      => {
+    id     => 'E1F958385BFE2B6E',
+    server => 'keys.gnupg.net'
+  }
+  location => 'https://packages.x2go.org/debian',
+  release  => $facts['os']['lsb']['distcodename'],
+  repos    => 'extras main',
+  include  => {
+    'src' => false,
+    'deb' => true
+  }
+}
+exec { 'apt-update':
+  command => 'apt-get update',
+  path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+}
+package { 'x2go-keyring':
+  ensure => latest,
+}
+exec { 'apt-update':
+  command => 'apt-get update',
+  path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+}
+package { 'x2goserver':
+  ensure => latest,
+}
+package { 'x2goserver-xsession':
+  ensure => latest,
+}
+package { 'x2godesktopsharing':
+  ensure => latest,
+}
