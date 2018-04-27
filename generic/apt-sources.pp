6de2140ad5c6fb2d632d5dcdc5efcbfc2d170b3f
scott <scott@tams.tech>
2018-04-26 21:10:13 -0400

    This is what happens if you don't pay close enough attention when copy-pasting.

--- generic/apt-sources.pp
+++ generic/apt-sources.pp
@@ -42,8 +42,8 @@ apt::source { 'distro':
   key      => $signing_keys[$lsbdistid]['updates']
 }
 
-apt::source { 'distro':
-  comment  => 'The default repository for the distro.',
+apt::source { 'distro-updates':
+  comment  => 'The distro\'s "updates" repository.',
   location => $mirror_link[$lsbdistid],
   release  => "${lsbdistcodename}-updates",
   repos    => $distro_suites[$lsbdistid],
@@ -54,8 +54,8 @@ apt::source { 'distro':
   key      => $signing_keys[$lsbdistid]['updates']
 }
 
-apt::source { 'distro':
-  comment  => 'The default repository for the distro.',
+apt::source { 'distro-security':
+  comment  => 'The security updates repository for the distro.',
   location => $mirror_link[$lsbdistid],
   release  => "${lsbdistcodename}-security",
   repos    => $distro_suites[$lsbdistid],
