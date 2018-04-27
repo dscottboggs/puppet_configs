03913bd1e8078dd1713625323251141175648f1f
scott <scott@tams.tech>
2018-04-26 21:07:09 -0400

    Fix hash syntax

--- generic/apt-sources.pp
+++ generic/apt-sources.pp
@@ -1,30 +1,30 @@
 $mirror_link = {
-  'Ubuntu' = 'http://us.archive.ubuntu.com/ubuntu/'
-  'Debian' = 'http://ftp.us.debian.org/debian/'
+  'Ubuntu' => 'http://us.archive.ubuntu.com/ubuntu/',
+  'Debian' => 'http://ftp.us.debian.org/debian/'
 }
 
 $distro_suites = {
-  'Ubuntu' = 'main restricted universe multiverse'
-  'Debian' = 'main contrib non-free'
+  'Ubuntu' => 'main restricted universe multiverse',
+  'Debian' => 'main contrib non-free'
 }
 $signing_keys = {
   'Debian' => {
     'updates' => {
       source => 'https://ftp-master.debian.org/keys/archive-key-9.asc',
       id     => 'E1CF20DDFFE4B89E802658F1E0B11894F66AEC98'
-    }
+    },
     'security' => {
       source => 'https://ftp-master.debian.org/keys/archive-key-9-security.asc',
       id     => '6ED6F5CB5FA6FB2F460AE88EEDA0D2388AE22BA9'
     }
-  }
+  },
   'Ubuntu' => {
     'updates' => {
-      source => 'https://keyserver.ubuntu.com'
+      source => 'https://keyserver.ubuntu.com',
       id     => '630239CC130E1A7FD81A27B140976EAF437D05B5'
-    }
+    },
     'security' => {
-      source => 'https://keyserver.ubuntu.com'
+      source => 'https://keyserver.ubuntu.com',
       id     => '630239CC130E1A7FD81A27B140976EAF437D05B5'
     }
   }
