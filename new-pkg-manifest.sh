f86081b4e3d6e25296b8f0a9fc491cdb0ba62657
scott <scott@tams.tech>
2018-04-28 00:17:20 -0400

    forgot to include the variables

--- new-pkg-manifest.sh
+++ new-pkg-manifest.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-puppet_dir=/root/puppet_configs/
+puppet_dir=/root/puppet_configs
 work_dir=$puppet_dir/package_installs
 pkg_dir=$work_dir/$1
 mkdir $pkg_dir
 ln -s $puppet_dir/generic/*.pp $pkg_dir/
+ln -s $puppet_dir/_variables.pp $pkg_dir/
 touch $pkg_dir/$1.pp
